72a2719e5b7e476ddfc86b4026d9e7c8f693e89c
asmaanassar <asmaanassar89@gmail.com>
2022-01-28 01:49:08 +0200

    newevent

--- apps.js
+++ apps.js
@@ -5,22 +5,22 @@
 let employeeForm = document.getElementById('employeeForm');
 let employeeSec = document.getElementById('employee');
 
-function Employee(employeeID, fullName, department, level,imgurl,salary) {
+function Employee(employeeID, fullName, department, level, imgurl, salary) {
     this.employeeID = employeeID;
     this.fullName = fullName;
     this.department = department;
     this.level = level;
-    this.imgurl = imgurl;  
-    this.salary = salary;
+    this.imgurl = imgurl;
+    this.salary = 0;
 }
 
-Employee.prototype.newEmployeeID = function(){
-    this.employeeID = getRndInteger(1000,9999);
+Employee.prototype.newEmployeeID = function () {
+    this.employeeID = getRndInteger(1000, 9999);
 }
 
 Employee.prototype.newSalary = function () {
     let netsalary;
-    
+
     if (this.level == 'Senior') {
         this.salary = getRndInteger(1500, 2000);
     } else if (this.level == 'Mid-Senior') {
@@ -29,7 +29,7 @@ Employee.prototype.newSalary = function () {
         this.salary = getRndInteger(500, 1000);
     }
 
-    netsalary= this.salary-(this.salary * 0.075);
+    netsalary = this.salary - (this.salary * 0.075);
     return netsalary;
 }
 
@@ -39,45 +39,48 @@ Employee.prototype.render = function () {
     employeeSec.appendChild(div);
 
 
-     let img = document.createElement('img');
-     div.appendChild(img);
-     img.setAttribute('src',this.imagePath);
-     img.setAttribute('alt',this.name);
+    let img = document.createElement('img');
+    div.appendChild(img);
+    if(this.imgurl != ''){
+    img.setAttribute('src',this.imgurl);
+    img.setAttribute('alt',this.fullName);
+    }else{
+        img.setAttribute('src',`./photo/${this.fullName}.jpg`);
+        img.setAttribute('alt',this.fullName);
+    }
 
-    //let img = document.createElement('img');
-    // div.appendChild(img);
-    // img.setAttribute('src',this.imgurl);
-    // img.setAttribute('alt',this.fullName);
-//     if(this.imgurl != ''){
-//     img.setAttribute('src',this.imgurl);
-//     img.setAttribute('alt',this.fullName);
-//  }else{
-//       img.setAttribute('src', this.imgurl);
-//     img.setAttribute('alt',this.fullName);
-//     // }
+    let p1 = document.createElement('p');
+    div.appendChild(p1);
+    p1.textContent = `Name: ${this.fullName}`
 
-    let p = document.createElement('p');
-    div.appendChild(p);
-    p.textContent = `Name: ${this.fullName} - ID: ${this.employeeID}`
-
-    let p2 =document.createElement('p');
+    let p2 = document.createElement('p');
     div.appendChild(p2);
-    p2.textContent =`Depertment: ${this.department} - Level: ${this.level}`
+    p2.textContent = `ID: ${this.employeeID}`
 
-    let p3 =document.createElement('p');
+    let p3 = document.createElement('p');
     div.appendChild(p3);
-    p3.textContent= `${this.salary}`
+    p3.textContent = `Depertment: ${this.department}`
+
+
+    let p4 = document.createElement('p');
+    div.appendChild(p4);
+    p4.textContent = `Level: ${this.level}`
+
+
+    let p5 = document.createElement('p');
+    div.appendChild(p5);
+    p5.textContent = `${this.salary}`
 
 }
 
-function handelSubmit(event){
+function handelSubmit(event) {
     event.preventDefault();
     let fullName = event.target.name.value;
 
     let department = event.target.department.value;
 
     let level = event.target.level.value;
-    
+
     let imgurl = event.target.imgurl.value;
 
 
